70dfbefe1a13166966ec563284543010cc22ded3
Zain Gulbaz <zaingulbaz8@gmail.com>
2023-01-18 13:18:46 +0500

    Slice and Splice

---
+++ Slice and Splice/Slice.js
@@ -0,0 +1,15 @@
+//For array slice method returns an array from start to the end-1
+
+let array = ["Zain", "Mehmood", "Taimur", "Babar", "Othman"];
+
+console.log(array.slice(-1, 4)); // []
+
+console.log(array.slice(2));
+
+console.log(array.slice(0, 3));
+
+let str = "Zain";
+
+console.log(str.slice(-1, 4)); //n
+console.log(str.slice(2)); //in
+console.log(str.slice(0, 3)); //Zai
---
+++ Slice and Splice/Splice.js
@@ -0,0 +1,12 @@
+//For array splice method returns an array from start to the end-1
+//It also effects the original array
+let array = ["Zain", "Mehmood", "Taimur", "Babar", "Othman"];
+
+// console.log(array.splice(0, 2)); // []
+
+console.log(array.splice(2));
+
+// console.log(array.splice(0, 3));
+
+let str = "Zain";
+//Spilce method does not work for strings
